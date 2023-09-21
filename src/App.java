8928226bc4966fa8363edd487f7ae9faf8a75eee
SaraNoori <sn1044604@student.francistuttle.edu>
2023-09-21 14:51:08 -0500

    finished postfix

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 //import java.util.ArrayList;
-
+/* 
 public class App {
     public static void main(String[] args) throws Exception {
         Integer[] array = {2, 1, 3};
@@ -24,10 +24,84 @@ public class App {
         System.out.println(s.peek());
         System.out.println();
 
+        // check if isEmpty works
         System.out.println(s.isEmpty());
 
         s = new Stack<Integer>();
         System.out.println(s.isEmpty());
 
     }
+}
+*/
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        
+        System.out.println(App.postfix("45+9/"));
+        System.out.println(App.postfix("23-5*"));
+
+    }
+
+
+public static int postfix(String exp)
+{
+    Stack<Integer> operands = new Stack<Integer>();
+
+    while (!exp.equals(""))
+    {
+        String bit = "";
+        bit = exp.substring(0, 1);
+        exp = exp.substring(1);
+        if (isNum(bit))
+        {
+            operands.push(Integer.parseInt(bit));
+        }
+        else
+        {
+            Integer second = operands.pop();
+            Integer first = operands.pop();
+
+            switch (bit)
+            {
+                case "*":
+                    operands.push(first * second);
+                break;
+
+                case "/":
+                    operands.push(first / second);
+                break;
+
+                case "+":
+                    operands.push(first + second);
+                break;
+
+                case "-":
+                    operands.push(first - second);
+                break;
+            }
+            
+        }
+    }
+    return operands.pop();
+
+    
+
+}
+
+
+// is it a number??
+public static boolean isNum(String inp)
+{
+    try
+    {
+        Integer.parseInt(inp);
+        return true;
+    }
+    catch (NumberFormatException e)
+    {
+        return false;
+    }
+}
 }
